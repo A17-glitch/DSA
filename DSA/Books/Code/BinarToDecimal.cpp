d373068c287d5a3d39d9e0976ccfc02957b196dc
a17-glitch <hossainalif412@gmail.com>
2025-09-07 12:19:12 +0600

    adding files

---
+++ DSA/Books/Code/BinarToDecimal.cpp
@@ -0,0 +1,26 @@
+// binary to decimal number conversion
+// source : apna college
+
+#include <iostream>
+using namespace std;
+int main() {
+  int decimal_number, ans = 0, pow = 1;
+
+  cout << "Enter yiur decimal number: ";
+  cin >> decimal_number;
+
+  int copy_decimal_number = decimal_number; // copying the decimal_number for
+                                            // showing it in the output later
+
+  while (decimal_number > 0) {
+
+    int reminder = decimal_number % 2;   // calculating reminders
+    decimal_number = decimal_number / 2; // updating decimal_number
+
+    ans += (reminder * pow);
+    pow = pow * 10;
+  }
+  cout << "\nThe Binary number of the Decimal number " << copy_decimal_number
+       << " is: " << ans << endl;
+  return 0;
+}
