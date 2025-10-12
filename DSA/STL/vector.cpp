ccf66f037b128e7b80f84bf9ae0329a4f2c427c1
A17-glitch <hossainalif412@gmail.com>
2025-10-12 20:18:11 +0600

    vector stl

---
+++ DSA/STL/vector.cpp
@@ -0,0 +1,31 @@
+
+/*
+
+vector
+
+
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+
+  //   vector<int> vec; // nothing here (0);
+  //   cout<<vec[0];
+
+  /*
+    size - cout<<vec.size()<<endl; //prints size
+    push_back() - vector.push_back() //element adds at last
+    pop_back() - vector.pop_back() //last element will be deleted
+    front() - cout<<vector.front()<<endl; //prints 1st value
+    back() - cout<<vector.back()<<endl; //prints last value
+    at() - vector(i) //accessing index value
+    capacity() - cout<<vector.capacity()<<endl; //prints capacity of the vector
+
+    vector capacity increases 2x as the size;
+  */
+  vector<int> v;
+
+  return 0;
+}
